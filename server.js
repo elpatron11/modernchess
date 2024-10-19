9f618ada574ddc52afda60bdc871b6ea22e575a4
Raul <cabaraul@gmail.com>
2024-10-19 18:37:58 -0400

    save1

--- server.js
+++ server.js
@@ -217,7 +217,7 @@ app.get('/generals', async (req, res) => {
 
 //game Clock
 // Schedule the countdown to reset every 2 hours at specific times
-schedule.scheduleJob('37 20 * * *', function() {
+schedule.scheduleJob('39 22 * * *', function() {
     console.log('Job triggered at:', new Date()); // Log the current time when job is triggered
     countdown = 7200; // reset countdown
     io.emit('countdown', { countdown });
