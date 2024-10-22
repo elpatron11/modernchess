82b2dadbe1b99cb4d66c33b63f214b991269952f
Raul <cabaraul@gmail.com>
2024-10-21 21:24:35 -0400

    testing timmer and db reset

--- server.js
+++ server.js
@@ -217,7 +217,7 @@ app.get('/generals', async (req, res) => {
 
 //game Clock
 // Schedule the countdown to reset every 2 hours at specific times
-schedule.scheduleJob('23 01 * * *', function() { //6pm server time
+schedule.scheduleJob('26 02 * * *', function() { //6pm server time
     console.log('Job triggered at:', new Date()); // Log the current time when job is triggered
     countdown = 7200; // reset countdown
     io.emit('countdown', { countdown });
