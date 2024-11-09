aa6df00f108faab0d004dd1f81c1eddbd2635ddd
Raul <cabaraul@gmail.com>
2024-11-09 11:34:03 -0500

    new general andinfo

--- public/script.js
+++ public/script.js
@@ -219,8 +219,8 @@ function loadGeneralDropdown() {
             const generals = playerData.ownedGenerals || []; // Assume 'generals' is an array of owned generals
             
               // Add default General Warrior (GW) if not already included
-              if (!generals.includes('Barbarian')) {
-                generals.unshift('Barbarian'); // Add GW at the start if it's missing
+              if (!generals.includes('GH')) {
+                generals.unshift('GH'); // Add GW at the start if it's missing
             }
 
             // Get the dropdown element
