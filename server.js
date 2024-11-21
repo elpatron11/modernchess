30683e36a9bd113842c85e82a4e9d7486f1a2bce
Raul <cabaraul@gmail.com>
2024-11-20 19:35:40 -0500

    fixed generals prices

--- server.js
+++ server.js
@@ -187,15 +187,15 @@ app.get('/generals', async (req, res) => {
         }
 
         const allGenerals = [
-            { name: 'GW', price: 0, gcPrice: 1000 },
-            { name: 'GH', price: 5, gcPrice: 2000 },
-            { name: 'GA', price: 5, gcPrice: 4000 },
-            { name: 'GM', price: 5, gcPrice: 8000 },
-            { name: 'Barbarian', price: 10, gcPrice: 8000 },
-            { name: 'Paladin', price: 10, gcPrice: 12000 },
-            { name: 'Voldemort', price: 15, gcPrice: 100000 },
-            { name: 'Orc', price: 10, gcPrice: 25000 },
-            { name: 'Robinhood', price: 15, gcPrice: 35000 },
+            { name: 'GW', price: 0, gcPrice: 500 },
+            { name: 'GH', price: 5, gcPrice: 600 },
+            { name: 'GA', price: 5, gcPrice: 700 },
+            { name: 'GM', price: 5, gcPrice: 800 },
+            { name: 'Barbarian', price: 10, gcPrice: 1000 },
+            { name: 'Paladin', price: 10, gcPrice: 1200 },
+            { name: 'Voldemort', price: 15, gcPrice: 25000 },
+            { name: 'Orc', price: 10, gcPrice: 1200 },
+            { name: 'Robinhood', price: 15, gcPrice: 25000 },
         ];
 
         const ownedGenerals = player.ownedGenerals || [];
